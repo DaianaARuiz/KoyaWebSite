627c48d9daa50fe3c33914d08833f43ef09aca9b
Daiana Ruiz <65079369+DaianaARuiz@users.noreply.github.com>
2020-11-09 00:19:59 -0300

    arreglos preguntas

--- js/preguntas.js
+++ js/preguntas.js
@@ -2,56 +2,58 @@
 
 const botonUno = document.getElementById('btn-FrequentQuestions_1');
 const respuestaUno = document.getElementById('response1');
+const clickUno = document.getElementById('play-boton_1');
 
-botonUno.addEventListener('click', ()=>{
+clickUno.addEventListener('click', ()=>{
     respuestaUno.classList.toggle('hidden-description');
-    botonUno.classList.toggle('fa-plus','fa-minus');
-    
-
+    botonUno.classList.toggle('fa-plus');
+    botonUno.classList.toggle('fa-minus');
 });
-
-document.getElementById('play-button').addEventListener('click', function(){
-    var icon = document.getElementById('icon');
-    icon.classList.toggle('fa-pause');
-    icon.classList.toggle('fa-play');
-  })
 
 //RESPUESTA 2 
 
 const botonDos = document.getElementById('btn-FrequentQuestions_2');
-const respuestaDos = document.getElementById('response2')
+const respuestaDos = document.getElementById('response2');
+const clickDos = document.getElementById('play-boton_2');
 
-botonDos.addEventListener('click', ()=>{
+clickDos.addEventListener('click', ()=>{
     respuestaDos.classList.toggle('hidden-description');
-    botonDos.classList.add('fa-minus');
+    botonDos.classList.toggle('fa-plus');
+    botonDos.classList.toggle('fa-minus');
 });
 
 //RESPUESTA 3 
 
 const botonTres = document.getElementById('btn-FrequentQuestions_3');
-const respuestaTres = document.getElementById('response3')
+const respuestaTres = document.getElementById('response3');
+const clickTres = document.getElementById('play-boton_3');
 
-botonTres.addEventListener('click', ()=>{
+clickTres.addEventListener('click', ()=>{
     respuestaTres.classList.toggle('hidden-description');
-    botonTres.classList.add('fa-minus');
+    botonTres.classList.toggle('fa-plus');
+    botonTres.classList.toggle('fa-minus');
 });
 
 //RESPUESTA 4 
 
 const botonCuatro = document.getElementById('btn-FrequentQuestions_4');
 const respuestaCuatro = document.getElementById('response4')
+const clickCuatro = document.getElementById('play-boton_4');
 
-botonCuatro.addEventListener('click', ()=>{
+clickCuatro.addEventListener('click', ()=>{
     respuestaCuatro.classList.toggle('hidden-description');
-    botonCuatro.classList.add('fa-minus');
+    botonCuatro.classList.toggle('fa-plus');
+    botonCuatro.classList.toggle('fa-minus');
 });
 
 //RESPUESTA 5 
 
 const botonCinco = document.getElementById('btn-FrequentQuestions_5');
-const respuestaCinco = document.getElementById('response5')
+const respuestaCinco = document.getElementById('response5');
+const clickCinco = document.getElementById('play-boton_5');
 
-botonCinco.addEventListener('click', ()=>{
+clickCinco.addEventListener('click', ()=>{
     respuestaCinco.classList.toggle('hidden-description');
-    botonCinco.classList.add('fa-minus');
+    botonCinco.classList.toggle('fa-plus');
+    botonCinco.classList.toggle('fa-minus');
 });
