a31170ded9615fa42f10e6ac238a103f0966b5e7
brian-crypto <britomflor066@gmail.com>
2020-11-19 20:08:23 -0300

    responsive19/11

--- js/index.js
+++ js/index.js
@@ -1,2 +1,5 @@
+document.getElementById('#icon').addEventListener('click', ()=>{
+    document.querySelector('.nav').classList.toggle('show');
+});
 ScrollReveal().reveal('.home');
 ScrollReveal().reveal('.home-informacion');
