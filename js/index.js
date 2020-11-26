b74566682b560943a64c58bd9b9c6186b509c53a
Daiana Ruiz <65079369+DaianaARuiz@users.noreply.github.com>
2020-11-26 14:17:40 -0300

    .

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
-const icon = document.getElementById('#icon');
+const icon = document.getElementById('icon');
 const nav = document.querySelector('.nav');
 
 icon.addEventListener('click', ()=>{
     nav.classList.toggle('show');
+
 });
+
 ScrollReveal().reveal('.home');
 ScrollReveal().reveal('.home-informacion');
